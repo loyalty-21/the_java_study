56e36dcbb1ea4126bc340f385b409be0ffeee9d6
dennis_21 <sanguine21@nate.com>
2021-06-19 11:27:06 +0900

    Initial commit

---
+++ src/main/java/me/loyalty21/the_java_study/TheJavaStudyApplication.java
@@ -0,0 +1,13 @@
+package me.loyalty21.the_java_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJavaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheJavaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/loyalty21/the_java_study/TheJavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loyalty21.the_java_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
